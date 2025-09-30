eb1d81409c60dafe3669d46141b861fa813160f6
arnaudcartuyvels@gmail.com <arnaudcartuyvels@gmail.com>
2025-09-30 14:43:16 +0000

    operational margin

---
+++ models/intermediate/int_orders_operational.sql
@@ -0,0 +1,19 @@
+with orders as (
+  select * from {{ ref('int_orders_margin') }}
+),
+ship as (
+  select
+    orders_id,
+    shipping_fee,
+    ship_cost
+  from {{ ref('stg_raw__ship') }}
+)
+
+select
+  o.orders_id,
+  o.date_date,
+  ROUND (o.margin
+    + coalesce(s.shipping_fee, 0)
+    - coalesce(s.ship_cost,  0),2)  as operational_margin
+from orders o
+left join ship s using (orders_id)
