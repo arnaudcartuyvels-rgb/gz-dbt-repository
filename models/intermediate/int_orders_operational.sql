384d7251b65195a0e4ebe0dc57809663cdd1dc8e
arnaudcartuyvels@gmail.com <arnaudcartuyvels@gmail.com>
2025-10-01 07:30:11 +0000

    adapted int_orders_operational with log_cost

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -5,15 +5,20 @@ ship as (
   select
     orders_id,
     shipping_fee,
-    ship_cost
+    ship_cost,
+    log_cost
   from {{ ref('stg_raw__ship') }}
 )
 
 select
   o.orders_id,
   o.date_date,
-  ROUND (o.margin
+  round(
+    o.margin
     + coalesce(s.shipping_fee, 0)
-    - coalesce(s.ship_cost,  0),2)  as operational_margin
+    - coalesce(s.log_cost,   0)
+    - coalesce(s.ship_cost,  0)
+  , 2) as operational_margin
 from orders o
 left join ship s using (orders_id)
+
