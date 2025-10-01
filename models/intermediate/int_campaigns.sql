3df081db171c774e073e59f9d5fc69587e4efa56
arnaudcartuyvels@gmail.com <arnaudcartuyvels@gmail.com>
2025-10-01 12:56:01 +0000

    first intermediate (silver) and schema update

---
+++ models/intermediate/int_campaigns.sql
@@ -0,0 +1,13 @@
+with all_campaigns as (
+
+    select * from {{ ref('stg_raw__adwords') }}
+    union all
+    select * from {{ ref('stg_raw__bing') }}
+    union all
+    select * from {{ ref('stg_raw__criteo') }}
+    union all
+    select * from {{ ref('stg_raw__facebook') }}
+
+)
+
+select * from all_campaigns
