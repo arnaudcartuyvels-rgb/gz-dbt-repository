bfdabe40b0b17d2d5eec324980e10eb7d46ee1f6
arnaudcartuyvels@gmail.com <arnaudcartuyvels@gmail.com>
2025-10-01 07:23:45 +0000

    forgot log cost

--- models/staging/raw/stg_raw__ship.sql
+++ models/staging/raw/stg_raw__ship.sql
@@ -1,12 +1,13 @@
 -- models/staging/raw/stg_raw__ship.sql
 with source as (
-  select * from {{ source('raw', 'ship') }}
+  select * from {{ source('raw','ship') }}
 ),
 renamed as (
   select
     orders_id,
-    shipping_fee,                       -- keep only one of the two
-    cast(ship_cost as float64) as ship_cost
+    shipping_fee,                             -- keep this one
+    cast(ship_cost as float64) as ship_cost,
+    cast(logCost  as float64) as log_cost     -- << new
   from source
 )
 select * from renamed
